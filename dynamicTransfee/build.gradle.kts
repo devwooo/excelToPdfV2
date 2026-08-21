a82b4916648581cd91cb3935dfe9a127b56a8967
devwooo <devacnt25@gmail.com>
2026-08-21 21:46:53 +0900

    동적 엑셀 TO PDF init()

---
+++ dynamicTransfee/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "hpy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // POI
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    // PDF
+    implementation("com.itextpdf:itextpdf:5.5.13.6")
+
+    //FONT
+    implementation("com.itextpdf:itext-asian:5.2.0");
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
